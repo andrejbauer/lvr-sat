5708251241ee348aee8cd6975e9a1bb7f113e173
Andrej Bauer <Andrej.Bauer@andrej.com>
2014-03-14 10:00:37 +0100

    CNF formule

--- bool.ml
+++ bool.ml
@@ -1,3 +1,5 @@
+(* Podatkovni tip za predstavitev boolovih izrazov *)
+
 type bool =
   | True
   | False
@@ -5,6 +7,29 @@ type bool =
   | Not of bool
   | Or of bool list
   | And of bool list
+
+(* Podatkovni tipi za predstavitev CNF izrazov *)
+
+type literal =
+  | Lit of string
+  | Til of string
+
+type clause = Clause of literal list
+
+type cnf = CNF of clause list
+
+(* Nekaj pomoznih funkcij *)
+
+let add x lst =  
+  if List.mem x lst then lst else x :: lst
+
+let rec del x = function
+  | [] -> []
+  | y :: lst -> if x = y then lst else y :: del x lst
+  
+let rec lookup x = function
+  | [] -> None
+  | (y,z) :: lst -> if x = y then Some z else lookup x lst
 
 (* Valuacijo predstavimo s seznamom parov (asociativni seznam)
    [(x1, b1); (x2, b2); ...; (xn, bn)]
@@ -19,7 +44,19 @@ let rec vrednost v p =
     | Or qs -> List.fold_left (fun b q -> b || vrednost v q) false qs
     | And qs -> List.fold_left (fun b q -> b && vrednost v q) true qs
 
-(* Negation normal form *)
+(* Spremenljivke, ki se pojavljajo v formuli *)
+let vars p =
+  let rec collect xs = function
+    | True -> xs
+    | False -> xs
+    | Var x -> add x xs
+    | Not p -> collect xs p
+    | And lst -> List.fold_left collect xs lst
+    | Or lst -> List.fold_left collect xs lst
+  in
+    collect [] p
+
+(* Tilation normal form *)
 let rec nnf p =
   match p with
     | True -> True
@@ -34,48 +71,36 @@ let rec nnf p =
     | Or qs -> Or (List.map nnf qs)
     | And qs -> And (List.map nnf qs)
 
-let rec simplify p =
-  let rec insert x lst =
-    match lst with
-      | [] -> [x]
-      | y :: ys ->
-        let cmp = compare x y in
-          if cmp < 0 then x :: lst
-          else if cmp = 0 then lst
-          else y :: (insert x ys)
-  in
-
-  let rec sort_uniq sorted unsorted =
-    match unsorted with
-      | [] -> sorted
-      | x :: xs -> sort_uniq (insert x sorted) xs
-  in        
-
-  match nnf p with
-    | False -> False
-    | True -> True
-    | Var x -> Var x
-    | Not (Var x) -> Not (Var x)
+(* Conjunctive normal form *)
+let cnf p =
+  let rec convert cs = function
+    | True -> cs
+    | False -> [Clause []]
+    | Var x -> (Clause [Lit x]) :: cs
+    | Not (Var x) -> (Clause [Til x]) :: cs
     | Not _ -> assert false
-    | Or qs ->
-      (* Poenostavimo qs, obrdžimo tiste, ki niso False, uredimo *)
-      let rs =
-        List.sort compare (List.filter (fun r -> r <> False) (List.map simplify qs))
-      in
-        (match rs with
-          | [] -> False
-          | [r] -> r
-          | rs when List.mem True rs -> True
-          | rs -> Or (sort_uniq [] rs)
-        )
-    | And qs ->
-      (* Poenostavimo qs, obrdžimo tiste, ki niso False, uredimo *)
-      let rs =
-        List.sort compare (List.filter (fun r -> r <> True) (List.map simplify qs))
-      in
-        (match rs with
-          | [] -> True
-          | [r] -> r
-          | rs when List.mem False rs -> False
-          | rs -> And (sort_uniq [] rs)
-        )
+    | And ps -> List.fold_left convert cs ps
+    | Or [] -> [Clause []]
+    | Or [p] -> convert cs p
+    | Or (p :: ps) ->
+      let ds = convert [] p in
+      let es = convert [] (Or ps) in
+        List.fold_left
+          (fun cs (Clause d) ->
+            List.fold_left (fun cs (Clause e) -> (Clause (d @ e)) :: cs) cs es)
+          cs ds
+  in
+    CNF (convert [] (nnf p))
+
+(* Spremenljivke v CNF formuli *)
+let vars_cnf (CNF cs) =
+  List.fold_left
+    (fun xs (Clause c) ->
+      List.fold_left
+        (fun xs l ->
+          match l with
+            | Lit x -> x :: xs
+            | Til x -> x :: xs)
+        xs c)
+    [] cs
+
